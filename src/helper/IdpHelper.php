7a20325c773ab4d56b3059f2004fa164dc7ada4d
Lorenzo Cattaneo <development@lorenzocattaneo.com>
2018-07-20 18:04:57 +0200

    changed idp metadata storage to xml

--- src/helper/IdpHelper.php
+++ src/helper/IdpHelper.php
@@ -7,7 +7,7 @@ class IdpHelper
         // Check if a file containing this idp metadata already exists
         $idpName = str_replace(".", "_", parse_url($idpUrl, PHP_URL_HOST));
         if (file_exists("../config/idp/" . $idpName)) {
-            $metadata = file_get_contents("../config/idp/" . $idpName);
+            $metadata = simplexml_load_file("../config/idp/" . $idpName . '.xml');
             return $metadata;
         }
 
@@ -22,15 +22,18 @@ class IdpHelper
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         $data = curl_exec($ch);
         curl_close($ch);
+
+        $dom = new DOMDocument();
+        $dom->loadXML($data);
+        $dom->save("../config/idp/" . $idpName . ".xml");
+
         $xml = new SimpleXMLElement($data);
         $metadata = array();
         $metadata['idp_entityid'] = $xml->xpath('//ns0:EntityDescriptor/@entityID')[0];
         $metadata['idp_sso'] = $xml->xpath('//ns0:SingleSignOnService/@Location')[0];
         $metadata['idp_slo'] = $xml->xpath('//ns0:SingleLogoutService/@Location')[0];
         $metadata['idp_cert'] = $xml->xpath('//ns1:X509Certificate')[0];
-
-        file_put_contents("../config/idp/" . $idpName, print_r($metadata, true));
-
+        
         return $metadata;
     }
 }
